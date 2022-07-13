03e65cb1f288294be476f6952c0836db26a7f207
jhonatanR08 <103544233+jhonatanR08@users.noreply.github.com>
2022-07-12 22:57:28 -0400

    Add files via upload

---
+++ ArbolGrafo.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmos1
+{
+    internal class ArbolGrafo
+    {
+        public NodoGrafo Raiz { get; set; }
+        public ArbolGrafo()
+        {
+            Raiz = null;
+        }
+        public void Agregar(NodoGrafo n)
+        {
+            NodoGrafo nodo = new NodoGrafo(n.Info);
+            Raiz = AgregarW(nodo, Raiz);
+        }
+
+        public NodoGrafo AgregarW(NodoGrafo ng, NodoGrafo raiz)
+        {
+            if (raiz == null)
+            {
+                raiz = ng;
+            }
+            else if(ng.Info < raiz.Info)
+            {
+                raiz.NodoIzq = AgregarW(ng, raiz.NodoIzq);
+            }
+            else
+            {
+                raiz.NodoDer = AgregarW(ng, raiz.NodoDer);
+
+            }
+            return raiz;
+        }
+
+        
+        public int Imprimir(NodoGrafo ng)
+        {
+            if (ng == null)
+            {
+                return 0;
+            }
+
+            //  [Izq | Info | Der ]
+            string  cad = string.Format("[{0}|{1}|{2}]", 
+                    ng.NodoIzq == null ?"null": ng.NodoIzq.Info.ToString(),
+                    ng.Info,
+                    ng.NodoDer == null ?"null": ng.NodoDer.Info.ToString());
+           
+            Console.WriteLine(cad);
+            Imprimir(ng.NodoIzq);
+            Imprimir(ng.NodoDer);
+            return 0;
+
+        }
+
+        // Implementar un algoritmo de busqueda en arboles binarios
+        public NodoGrafo Buscar(int buscar)
+        {
+            NodoGrafo ng = null;
+            return ng;
+        }
+        public void buscar(int x, NodoGrafo t)
+        {
+            if (t != null)
+            {
+                if (x < t.Info)
+                {
+                    buscar(x, t.NodoIzq);
+                }
+                else
+                {
+                    if (x > t.Info)
+                    {
+                        buscar(x, t.NodoDer);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nodo no encontrado en el arbol", "Error de busqueda");
+            }
+            
+        }
+        
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmos1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejemplo arbol binario");
+            ArbolGrafo arbol = new ArbolGrafo();
+            arbol.Agregar(new NodoGrafo(5));
+            arbol.Agregar(new NodoGrafo(4));
+            arbol.Agregar(new NodoGrafo(3));
+            arbol.Agregar(new NodoGrafo(2));
+            arbol.Agregar(new NodoGrafo(1));
+            arbol.Agregar(new NodoGrafo(16));
+            arbol.Agregar(new NodoGrafo(8));
+            arbol.Agregar(new NodoGrafo(6));
+            arbol.Agregar(new NodoGrafo(9));
+            arbol.Agregar(new NodoGrafo(22));
+            arbol.Agregar(new NodoGrafo(17));
+            arbol.Agregar(new NodoGrafo(25));
+            arbol.Agregar(new NodoGrafo(23));
+
+            arbol.Imprimir(arbol.Raiz);
+
+            Console.WriteLine("Ingrese elemento a buscar ");
+            int x = int.Parse(Console.ReadLine());
+            NodoGrafo nb = arbol.Buscar(x);
+            Console.WriteLine("Encontrado ");
+            Console.WriteLine(NodoDer + "|" + x +"|"+ NodoIzq);
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+
+        public static object NodoDer { get; set; }
+
+        public static int NodoIzq { get; set; }
+    }
+}
